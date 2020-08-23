3da3f36a3b1194f144802226a888539a881fd460
Udaya Prakash N <udayaprakash2899@gmail.com>
2020-08-23 13:58:51 +0530

    First commit

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="w-full h-full">
+            <div>
+                <img src="" alt=""/>
+            </div>
+            <div>Div 2</div>
+        </div>
+    )
+}
+
+export default Home
