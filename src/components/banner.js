e0d8d37785b3530e47ccf76f681cf1df50504738
Udaya Prakash N <udayaprakash2899@gmail.com>
2020-08-31 03:48:06 +0530

    Basic responsivenesS

--- src/components/banner.js
+++ src/components/banner.js
@@ -4,12 +4,12 @@ import codeInspection from '../assets/code_inspection.png'
 
 const Banner = () => {
     return (
-        <div className="flex flex-col md:flex-row w-full h-full items-center">
+        <div className="md:flex md:p-8 md:mt-4 w-full h-full md:items-center">
                 <div className="flex flex-col md:flex-1 h-full text-steel-teal text-3xl opacity-75 font-bold cursor-default justify-center">
-                    <div className="">
-                        <img className="m-auto" src={codeInspection} width={200} alt=""/>
+                    <div className="sm:flex sm:justify-center">
+                        <img className="" width={400} src={codeInspection}  alt=""/>
                     </div>
-                    <div>
+                    <div className="sm:m-auto">
                     Take{" "} 
                     <span className="bg-underline">
                         aesthetic
@@ -17,7 +17,7 @@ const Banner = () => {
                     {" "}screenshots of
                     <Typed
                     className="font-code font-regular text-burnt-sienna text-5xl block"
-                    strings={['snippets', 'functions', 'code']}
+                    strings={['snippets.', 'functions.', 'code.']}
                     typeSpeed={120}
                     backSpeed={35}
                     smartBackspace={true}
@@ -26,7 +26,7 @@ const Banner = () => {
                     
                     
                 </div>
-                <div className="md:flex-1">
+                <div className="md:flex md:flex-1 h-full mt-12 md:items-center">
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus laudantium, nemo hic iste tempore iusto cupiditate similique doloremque voluptatibus ea exercitationem fuga, temporibus impedit libero. Nesciunt corrupti architecto ratione cumque?
                 </div>
             </div>
