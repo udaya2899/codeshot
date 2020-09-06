14921b85dda7f60a8ac38c0ea13009bf9e70589a
Udaya Prakash N <udayaprakash2899@gmail.com>
2020-09-07 01:35:33 +0530

    :sparkles: added shadow and html2canvas

--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -1,10 +1,31 @@
 import React from "react";
 import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/theme-dracula";
+import html2canvas from "html2canvas";
+
+const TestButton = () => {
+  html2canvas(document.querySelector("#capture")).then((canvas) => {
+    window.open(canvas.toDataURL("image/png"));
+  });
+};
 
 const CodeEditor = () => {
   return (
-    <div className="md:flex md:flex-1 h-full mt-12 md:items-center">
-      <AceEditor />
+    <div className="md:flex md:flex-1 p-4 h-full mt-12 md:items-center md:flex-col">
+      <div id="capture">
+        <AceEditor
+          mode="java"
+          theme="monokai"
+          className="w-full h-full code-shadow"
+          showGutter={false}
+        />
+      </div>
+
+      <div className="mt-12">
+        <button onClick={TestButton}>Export</button>
+      </div>
     </div>
   );
 };
